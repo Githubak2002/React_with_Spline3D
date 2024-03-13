0f0cd9a184a346dfb9b106ed12152736175e6816
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2024-03-14 04:45:14 +0530

    updated - Spline 3D tshirt fro phone screen

--- src/components/Spline_tshirt.jsx
+++ src/components/Spline_tshirt.jsx
@@ -1,9 +1,9 @@
-import Spline from '@splinetool/react-spline';
+import Spline from "@splinetool/react-spline";
 
 export default function Spline_tshirt() {
   return (
-    <main>
-    <Spline scene="https://prod.spline.design/OH7uJtp8TWSVrRN9/scene.splinecode" />
+    <main className="h-screen">
+      <Spline scene="https://prod.spline.design/OH7uJtp8TWSVrRN9/scene.splinecode" />
     </main>
   );
 }
